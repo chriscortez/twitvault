7cb61b9a8cec5126d78f835502abb527e0a7ae07
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-04-27 16:16:42 -0500

    added twitvault module and adjusted the path for require

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var twitter = require('twitvault-twitter');
+//var twitter = require('twitvault-twitter');
+var twitter = require('../src/modules/twitvault-twitter');
 
 router.get('/', function(req, res) {
   twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
     if (error) {
       console.log("Error getting OAuth request token : " + error);
     } else {
+      console.log("got request token %s\n", requestToken);
       global.requestToken = requestToken;
       global.requestTokenSecret = requestTokenSecret;
       res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + global.requestToken);
